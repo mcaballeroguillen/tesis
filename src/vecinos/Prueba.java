ac4af63b28e9d1aa8f2494b2e4e24c5d67985bb7
mcaballeroguillen <mcaballeroguillen@gmail.com>
2018-12-10 17:53:02 -0300

    manejando datos

--- src/vecinos/Prueba.java
+++ src/vecinos/Prueba.java
@@ -14,15 +14,14 @@ public class Prueba {
  public static void main(String[] args) throws IOException{
 	 InputStream in = null;
 	 ArrayList<Node[]> movies = new ArrayList<Node[]>();
-	 in = new FileInputStream("/home/marco/WIKIDATA/wikidata-20181123-truthy-BETA.nt.gz");
+	 in = new FileInputStream("/u/m/mag/2017/mcaballe/Descargas/wikidata-20181123-truthy-BETA.nt.gz");
 	 in = new GZIPInputStream(in);
 	 NxParser nxp = new NxParser();
 	 nxp.parse(in);
-	 int x=0;
 	 
-	
+	 System.out.println("Vamos a ver");
 	 for (Node[] nx : nxp){
-		 if(nx[1].equals("<http://www.wikidata.org/prop/direct/P31>") && nx[2].equals("<http://www.wikidata.org/entity/Q11424>")){
+		 if(nx[1].equals("<http://www.wikidata.org/prop/direct/P31>") ){
 			 movies.add(nx);
 		 }
 	
