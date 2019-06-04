004807ded9d4d9c0b4e6275395d62ef6252156e4
mcaballeroguillen <mcaballeroguillen@gmail.com>
2019-06-04 17:01:08 -0400

    peliculas mucho, pasamos a pelis

--- src/vecinos/Main.java
+++ src/vecinos/Main.java
@@ -10,15 +10,15 @@ public class Main {
 				System.out.println("Debe ingresar: Dirección WIKIDATA, Directorio de Trabajo, Tipo de la busqueda y límite");
 				System.exit(-1);
 			}
-			System.out.println("Star");
-			Extract extractor = new  Extract(args[0],Integer.valueOf(Integer.valueOf(args[3])));
-			extractor.FindObjectes(args[1]+"/result.txt",args[2]);
-			System.out.println("Vecinos O(N)");
-			ExtractNeightbor extract1 = new ExtractNeightbor(args[1]);
-			extract1.extract();
-			System.out.println("Vecinos O(N^2)");
-			ExtractInterseption extract2 = new ExtractInterseption(args[1]);
-			extract2.extract();
+			//System.out.println("Star");
+			//Extract extractor = new  Extract(args[0],Integer.valueOf(Integer.valueOf(args[3])));
+			//extractor.FindObjectes(args[1]+"/result.txt",args[2]);
+			//System.out.println("Vecinos O(N)");
+			//ExtractNeightbor extract1 = new ExtractNeightbor(args[1]);
+			//extract1.extract();
+			//System.out.println("Vecinos O(N^2)");
+			//ExtractInterseption extract2 = new ExtractInterseption(args[1]);
+			//extract2.extract();
 			System.out.println("Vecinos O(N^3)");
 			Extract3Interseption extract3 = new Extract3Interseption(args[1]);
 			extract3.extract();
