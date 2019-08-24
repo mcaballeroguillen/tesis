50ed2188f6a468591f46a85f667f1ada0178f1d6
mcaballeroguillen <mcaballeroguillen@gmail.com>
2019-08-24 16:42:49 -0400

    pruebas de kendalls termindas

--- scripts/scr_ke.sh
+++ scripts/scr_ke.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/comidas/result_norm_4/rank1.txt  /home/mcaballero/pruebas/comidas/result_norm_500/rank1.txt /home/mcaballero/pruebas/comidas/result_norm_4
+java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/modelos_autos/result_norm_4/rank1.txt  /home/mcaballero/pruebas/modelos_autos/result_norm_500/rank1.txt /home/mcaballero/pruebas/modelos_autos/result_norm_4
 
-java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/comidas/result_norm_8/rank1.txt  /home/mcaballero/pruebas/comidas/result_norm_500/rank1.txt /home/mcaballero/pruebas/comidas/result_norm_8
+java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/modelos_autos/result_norm_8/rank1.txt  /home/mcaballero/pruebas/modelos_autos/result_norm_500/rank1.txt /home/mcaballero/pruebas/modelos_autos/result_norm_8
 
-java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/comidas/result_norm_16/rank1.txt  /home/mcaballero/pruebas/comidas/result_norm_500/rank1.txt /home/mcaballero/pruebas/comidas/result_norm_16
+java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/modelos_autos/result_norm_16/rank1.txt  /home/mcaballero/pruebas/modelos_autos/result_norm_500/rank1.txt /home/mcaballero/pruebas/modelos_autos/result_norm_16
 
-java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/comidas/result_norm_32/rank1.txt  /home/mcaballero/pruebas/comidas/result_norm_500/rank1.txt /home/mcaballero/pruebas/comidas/result_norm_32
+java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/modelos_autos/result_norm_32/rank1.txt  /home/mcaballero/pruebas/modelos_autos/result_norm_500/rank1.txt /home/mcaballero/pruebas/modelos_autos/result_norm_32
 
-java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/comidas/result_norm_64/rank1.txt  /home/mcaballero/pruebas/comidas/result_norm_500/rank1.txt /home/mcaballero/pruebas/comidas/result_norm_64
+java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/modelos_autos/result_norm_64/rank1.txt  /home/mcaballero/pruebas/modelos_autos/result_norm_500/rank1.txt /home/mcaballero/pruebas/modelos_autos/result_norm_64
 
-java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/comidas/result_norm_128/rank1.txt  /home/mcaballero/pruebas/comidas/result_norm_500/rank1.txt /home/mcaballero/pruebas/comidas/result_norm_128
+java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/modelos_autos/result_norm_128/rank1.txt  /home/mcaballero/pruebas/modelos_autos/result_norm_500/rank1.txt /home/mcaballero/pruebas/modelos_autos/result_norm_128
 
-java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/comidas/result_norm_256/rank1.txt  /home/mcaballero/pruebas/comidas/result_norm_500/rank1.txt /home/mcaballero/pruebas/comidas/result_norm_256
+java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/modelos_autos/result_norm_256/rank1.txt  /home/mcaballero/pruebas/modelos_autos/result_norm_500/rank1.txt /home/mcaballero/pruebas/modelos_autos/result_norm_256
 
 
