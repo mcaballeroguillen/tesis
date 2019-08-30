3be55ad00f204c1cfbb4b2b981db64dec830b78f
mcaballeroguillen <mcaballeroguillen@gmail.com>
2019-08-30 18:17:02 -0400

    pruebas prob terminadas

--- scripts/scr_for.sh
+++ scripts/scr_for.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-java -Xms10G -jar format.jar /home/mcaballero/pruebas/paises/result_probalistic_4/union 1 /home/mcaballero/pruebas/paises/result_probalistic_4/
+java -Xms10G -jar format.jar /home/mcaballero/pruebas/comidas/result_probalistic_4/union 1 /home/mcaballero/pruebas/comidas/result_probalistic_4/
 
-java -Xms10G -jar format.jar /home/mcaballero/pruebas/paises/result_probalistic_8/union 1 /home/mcaballero/pruebas/paises/result_probalistic_8/
+java -Xms10G -jar format.jar /home/mcaballero/pruebas/comidas/result_probalistic_8/union 1 /home/mcaballero/pruebas/comidas/result_probalistic_8/
 
-java -Xms10G -jar format.jar /home/mcaballero/pruebas/paises/result_probalistic_16/union 1 /home/mcaballero/pruebas/paises/result_probalistic_16/
+java -Xms10G -jar format.jar /home/mcaballero/pruebas/comidas/result_probalistic_16/union 1 /home/mcaballero/pruebas/comidas/result_probalistic_16/
 
-java -Xms10G -jar format.jar /home/mcaballero/pruebas/paises/result_probalistic_32/union 1 /home/mcaballero/pruebas/paises/result_probalistic_32/
+java -Xms10G -jar format.jar /home/mcaballero/pruebas/comidas/result_probalistic_32/union 1 /home/mcaballero/pruebas/comidas/result_probalistic_32/
 
-java -Xms10G -jar format.jar /home/mcaballero/pruebas/paises/result_probalistic_64/union 1 /home/mcaballero/pruebas/paises/result_probalistic_64/
+java -Xms10G -jar format.jar /home/mcaballero/pruebas/comidas/result_probalistic_64/union 1 /home/mcaballero/pruebas/comidas/result_probalistic_64/
 
-java -Xms10G -jar format.jar /home/mcaballero/pruebas/paises/result_probalistic_128/union 1 /home/mcaballero/pruebas/paises/result_probalistic_128/
+java -Xms10G -jar format.jar /home/mcaballero/pruebas/comidas/result_probalistic_128/union 1 /home/mcaballero/pruebas/comidas/result_probalistic_128/
 
-java -Xms10G -jar format.jar /home/mcaballero/pruebas/paises/result_probalistic_256/union 1 /home/mcaballero/pruebas/paises/result_probalistic_256/
+java -Xms10G -jar format.jar /home/mcaballero/pruebas/comidas/result_probalistic_256/union 1 /home/mcaballero/pruebas/comidas/result_probalistic_256/
 
-java -Xms10G -jar format.jar /home/mcaballero/pruebas/paises/result_probalistic_500/union 1 /home/mcaballero/pruebas/paises/result_probalistic_500/
+java -Xms10G -jar format.jar /home/mcaballero/pruebas/comidas/result_probalistic_500/union 1 /home/mcaballero/pruebas/comidas/result_probalistic_500/
 
 
--- scripts/scr_ke.sh
+++ scripts/scr_ke.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/paises/result_probalistic_4/rank1.txt  /home/mcaballero/pruebas/paises/result_probalistic_500/rank1.txt /home/mcaballero/pruebas/paises/result_probalistic_4
+java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/comidas/result_probalistic_4/rank1.txt  /home/mcaballero/pruebas/comidas/result_probalistic_500/rank1.txt /home/mcaballero/pruebas/comidas/result_probalistic_4
 
-java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/paises/result_probalistic_8/rank1.txt  /home/mcaballero/pruebas/paises/result_probalistic_500/rank1.txt /home/mcaballero/pruebas/paises/result_probalistic_8
+java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/comidas/result_probalistic_8/rank1.txt  /home/mcaballero/pruebas/comidas/result_probalistic_500/rank1.txt /home/mcaballero/pruebas/comidas/result_probalistic_8
 
-java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/paises/result_probalistic_16/rank1.txt  /home/mcaballero/pruebas/paises/result_probalistic_500/rank1.txt /home/mcaballero/pruebas/paises/result_probalistic_16
+java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/comidas/result_probalistic_16/rank1.txt  /home/mcaballero/pruebas/comidas/result_probalistic_500/rank1.txt /home/mcaballero/pruebas/comidas/result_probalistic_16
 
-java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/paises/result_probalistic_32/rank1.txt  /home/mcaballero/pruebas/paises/result_probalistic_500/rank1.txt /home/mcaballero/pruebas/paises/result_probalistic_32
+java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/comidas/result_probalistic_32/rank1.txt  /home/mcaballero/pruebas/comidas/result_probalistic_500/rank1.txt /home/mcaballero/pruebas/comidas/result_probalistic_32
 
-java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/paises/result_probalistic_64/rank1.txt  /home/mcaballero/pruebas/paises/result_probalistic_500/rank1.txt /home/mcaballero/pruebas/paises/result_probalistic_64
+java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/comidas/result_probalistic_64/rank1.txt  /home/mcaballero/pruebas/comidas/result_probalistic_500/rank1.txt /home/mcaballero/pruebas/comidas/result_probalistic_64
 
-java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/paises/result_probalistic_128/rank1.txt  /home/mcaballero/pruebas/paises/result_probalistic_500/rank1.txt /home/mcaballero/pruebas/paises/result_probalistic_128
+java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/comidas/result_probalistic_128/rank1.txt  /home/mcaballero/pruebas/comidas/result_probalistic_500/rank1.txt /home/mcaballero/pruebas/comidas/result_probalistic_128
 
-java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/paises/result_probalistic_256/rank1.txt  /home/mcaballero/pruebas/paises/result_probalistic_500/rank1.txt /home/mcaballero/pruebas/paises/result_probalistic_256
+java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/comidas/result_probalistic_256/rank1.txt  /home/mcaballero/pruebas/comidas/result_probalistic_500/rank1.txt /home/mcaballero/pruebas/comidas/result_probalistic_256
 
 
