dcc6a4308b2dce5c0cca8a3cf38a7e9abf24c7cc
mcaballeroguillen <mcaballeroguillen@gmail.com>
2019-09-05 18:12:57 -0400

    falta última prueba

--- scripts/scr_ke.sh
+++ scripts/scr_ke.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/universidades/interseption3_4/incluidos/rank2.txt  /home/mcaballero/pruebas/universidades/interseption3_500/incluidos/rank2.txt /home/mcaballero/pruebas/universidades/interseption3_4
+java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/paises/interseption3_4/incluidos/rank2.txt  /home/mcaballero/pruebas/paises/interseption3_500/incluidos/rank2.txt /home/mcaballero/pruebas/paises/interseption3_4
 
-java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/universidades/interseption3_8/incluidos/rank2.txt  /home/mcaballero/pruebas/universidades/interseption3_500/incluidos/rank2.txt /home/mcaballero/pruebas/universidades/interseption3_8
+java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/paises/interseption3_8/incluidos/rank2.txt  /home/mcaballero/pruebas/paises/interseption3_500/incluidos/rank2.txt /home/mcaballero/pruebas/paises/interseption3_8
 
-java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/universidades/interseption3_16/incluidos/rank2.txt  /home/mcaballero/pruebas/universidades/interseption3_500/incluidos/rank2.txt /home/mcaballero/pruebas/universidades/interseption3_16
+java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/paises/interseption3_16/incluidos/rank2.txt  /home/mcaballero/pruebas/paises/interseption3_500/incluidos/rank2.txt /home/mcaballero/pruebas/paises/interseption3_16
 
-java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/universidades/interseption3_32/incluidos/rank2.txt  /home/mcaballero/pruebas/universidades/interseption3_500/incluidos/rank2.txt /home/mcaballero/pruebas/universidades/interseption3_32
+java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/paises/interseption3_32/incluidos/rank2.txt  /home/mcaballero/pruebas/paises/interseption3_500/incluidos/rank2.txt /home/mcaballero/pruebas/paises/interseption3_32
 
-java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/universidades/interseption3_64/incluidos/rank2.txt  /home/mcaballero/pruebas/universidades/interseption3_500/incluidos/rank2.txt /home/mcaballero/pruebas/universidades/interseption3_64
+java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/paises/interseption3_64/incluidos/rank2.txt  /home/mcaballero/pruebas/paises/interseption3_500/incluidos/rank2.txt /home/mcaballero/pruebas/paises/interseption3_64
 
-java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/universidades/interseption3_128/incluidos/rank2.txt  /home/mcaballero/pruebas/universidades/interseption3_500/incluidos/rank2.txt /home/mcaballero/pruebas/universidades/interseption3_128
+java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/paises/interseption3_128/incluidos/rank2.txt  /home/mcaballero/pruebas/paises/interseption3_500/incluidos/rank2.txt /home/mcaballero/pruebas/paises/interseption3_128
 
-java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/universidades/interseption3_256/incluidos/rank2.txt  /home/mcaballero/pruebas/universidades/interseption3_500/incluidos/rank2.txt /home/mcaballero/pruebas/universidades/interseption3_256
+java -Xms15G -jar kendalls.jar /home/mcaballero/pruebas/paises/interseption3_256/incluidos/rank2.txt  /home/mcaballero/pruebas/paises/interseption3_500/incluidos/rank2.txt /home/mcaballero/pruebas/paises/interseption3_256
--- scripts/scr_t.sh
+++ scripts/scr_t.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-java -jar -Xms15G triple_4.jar /home/mcaballero/datos/wikidata-20190704-truthy-BETA.nt.gz /home/mcaballero/pruebas/universidades "<http://www.wikidata.org/entity/Q3231690>" 0 &> salida4_triple
+java -jar -Xms15G triple_4.jar /home/mcaballero/datos/wikidata-20190704-truthy-BETA.nt.gz /home/mcaballero/pruebas/comidas "<http://www.wikidata.org/entity/Q3231690>" 0 &> salida4_triple
 
-java -jar -Xms15G triple_8.jar /home/mcaballero/datos/wikidata-20190704-truthy-BETA.nt.gz /home/mcaballero/pruebas/universidades "<http://www.wikidata.org/entity/Q3231690>" 0 &> salida8_triple
+java -jar -Xms15G triple_8.jar /home/mcaballero/datos/wikidata-20190704-truthy-BETA.nt.gz /home/mcaballero/pruebas/comidas "<http://www.wikidata.org/entity/Q3231690>" 0 &> salida8_triple
 
-java -jar -Xms15G triple_16.jar /home/mcaballero/datos/wikidata-20190704-truthy-BETA.nt.gz /home/mcaballero/pruebas/universidades "<http://www.wikidata.org/entity/Q3231690>" 0 &> salida16_triple
+java -jar -Xms15G triple_16.jar /home/mcaballero/datos/wikidata-20190704-truthy-BETA.nt.gz /home/mcaballero/pruebas/comidas "<http://www.wikidata.org/entity/Q3231690>" 0 &> salida16_triple
 
-java -jar -Xms15G triple_32.jar /home/mcaballero/datos/wikidata-20190704-truthy-BETA.nt.gz /home/mcaballero/pruebas/universidades "<http://www.wikidata.org/entity/Q3231690>" 0 &> salida32_triple
+java -jar -Xms15G triple_32.jar /home/mcaballero/datos/wikidata-20190704-truthy-BETA.nt.gz /home/mcaballero/pruebas/comidas "<http://www.wikidata.org/entity/Q3231690>" 0 &> salida32_triple
 
-java -jar -Xms15G triple_64.jar /home/mcaballero/datos/wikidata-20190704-truthy-BETA.nt.gz /home/mcaballero/pruebas/universidades "<http://www.wikidata.org/entity/Q3231690>" 0 &> salida64_triple
+java -jar -Xms15G triple_64.jar /home/mcaballero/datos/wikidata-20190704-truthy-BETA.nt.gz /home/mcaballero/pruebas/comidas "<http://www.wikidata.org/entity/Q3231690>" 0 &> salida64_triple
 
-java -jar -Xms15G triple_128.jar /home/mcaballero/datos/wikidata-20190704-truthy-BETA.nt.gz /home/mcaballero/pruebas/universidades "<http://www.wikidata.org/entity/Q3231690>" 0 &> salida128_triple
+java -jar -Xms15G triple_128.jar /home/mcaballero/datos/wikidata-20190704-truthy-BETA.nt.gz /home/mcaballero/pruebas/comidas "<http://www.wikidata.org/entity/Q3231690>" 0 &> salida128_triple
 
-java -jar -Xms15G triple_256.jar /home/mcaballero/datos/wikidata-20190704-truthy-BETA.nt.gz /home/mcaballero/pruebas/universidades "<http://www.wikidata.org/entity/Q3231690>" 0 &> salida256_triple
+java -jar -Xms15G triple_256.jar /home/mcaballero/datos/wikidata-20190704-truthy-BETA.nt.gz /home/mcaballero/pruebas/comidas "<http://www.wikidata.org/entity/Q3231690>" 0 &> salida256_triple
 
-java -jar -Xms15G triple_500.jar /home/mcaballero/datos/wikidata-20190704-truthy-BETA.nt.gz /home/mcaballero/pruebas/universidades "<http://www.wikidata.org/entity/Q3231690>" 0 &> salida500_triple
+java -jar -Xms15G triple_500.jar /home/mcaballero/datos/wikidata-20190704-truthy-BETA.nt.gz /home/mcaballero/pruebas/comidas "<http://www.wikidata.org/entity/Q3231690>" 0 &> salida500_triple
